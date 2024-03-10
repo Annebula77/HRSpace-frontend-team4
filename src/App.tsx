7b6df1179271548d242a2be480f8a560add42922
MaximNabiulin <nabiulin.m.kh@gmail.com>
2024-03-10 23:31:16 +0500

    HH-7: add docker settings

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -27,7 +27,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
