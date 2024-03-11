5e89ed07215cc9cadcb478a9d161c30d2d52dba5
goharvard77 <goharvard77@gmail.com>
2024-03-11 13:20:11 +0300

    HH-4: fix linting

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,35 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" aria-label="Learn more about Vite">Vite</a>
+        <a href="https://react.dev" aria-label="Learn more about React">React</a>
 
-        </a>
-        <a href="https://react.dev" target="_blank">
-
-        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button type="button" onClick={() => setCount((currentCount) => currentCount + 1)}>
+          count is
+          {' '}
+          {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
