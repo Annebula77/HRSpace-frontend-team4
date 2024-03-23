beb32a2deb2bc4df5d1f9817c346de0fdecbc478
MaximNabiulin <nabiulin.m.kh@gmail.com>
2024-03-23 11:47:30 +0500

    HH-45: add durger menu for header & adaptive

---
+++ src/components/burgerMenu/BurgerMenu.tsx
@@ -0,0 +1,145 @@
+import { type FC, useEffect } from 'react';
+import styled from 'styled-components';
+
+import HeaderUserInfo from '../headerUserInfo/HeaderUserInfo';
+import CloseButtonIcon from '../icons/CloseButtonIcon';
+
+interface BurgerMenuProps {
+  isOpen: boolean,
+  onClose: () => void,
+}
+
+interface OverlayProps {
+  onClick: (e: React.MouseEvent<Element, MouseEvent>) => void,
+  isOpen: boolean,
+  children: React.ReactNode,
+}
+
+interface ButtonProps {
+  $primary?: boolean;
+}
+
+const StyledBurgerMenuOverlay: FC<OverlayProps> = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  visibility: ${(props) => (props.isOpen ? 'visible' : 'hidden')};
+  opacity: ${(props) => (props.isOpen ? '1' : '0')};
+  transition: visibility 0.3s, opacity 0.3s linear;
+  z-index: 2;
+`;
+
+const StyledBurgerMenuContent = styled.div`
+  /* max-width: 450px; */
+  margin: 0;
+  width: 70%;
+  /* max-height: 50%; */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: rgba(255, 255, 255, 1);
+  z-index: 3;
+  /* border-radius: 10px; */
+
+  & button {
+    align-self: flex-end;
+    margin: 15px 0 0;
+    border: none;
+    background-color: rgba(0, 0, 0, 0);
+    cursor: pointer;
+    transition: opacity 0.1s;
+  }
+
+  & button:hover {
+    opacity: 0.6;
+  }
+`;
+
+const StyledList = styled.ul<ButtonProps>`
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+
+  & li {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+    }
+
+  & li:last-child {
+    margin: 0;
+  }
+`;
+
+const StyledNavButton = styled.button<ButtonProps>`
+  margin: 0;
+  padding: 0;
+  font-size: 16px;
+  font-weight: ${({ $primary }) => ($primary ? '700' : '400')};
+  line-height: 24px;
+  letter-spacing: 0px;
+  text-align: left;
+  border: none;
+  background-color: rgba(0, 0, 0, 0);
+`;
+
+const BurgerMenu: FC<BurgerMenuProps> = ({ isOpen, onClose }) => {
+  useEffect(() => {
+    if (!isOpen) return;
+    const closeByEscape = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
+    document.addEventListener('keydown', closeByEscape);
+    // eslint-disable-next-line consistent-return
+    return () => {
+      document.removeEventListener('keydown', closeByEscape);
+    };
+  }, [isOpen, onClose]);
+
+  const handleOverlay = (event: React.MouseEvent<Element, MouseEvent>) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
+  return (
+    <StyledBurgerMenuOverlay onClick={handleOverlay} isOpen={isOpen} aria-hidden="true">
+      <StyledBurgerMenuContent>
+        <button id="close-button" type="button" onClick={onClose} aria-label="close-button">
+          <CloseButtonIcon />
+        </button>
+        <HeaderUserInfo isBurgerMenu />
+        <nav>
+          <StyledList>
+            <li>
+              <StyledNavButton type="button" $primary>Создать заявку</StyledNavButton>
+            </li>
+            <li>
+              <StyledNavButton type="button">Мои заявки</StyledNavButton>
+            </li>
+            <li>
+              <StyledNavButton type="button">Поиск по рынку</StyledNavButton>
+            </li>
+            <li>
+              <StyledNavButton type="button">Счёт</StyledNavButton>
+            </li>
+            <li>
+              <StyledNavButton type="button">Помощь</StyledNavButton>
+            </li>
+          </StyledList>
+        </nav>
+      </StyledBurgerMenuContent>
+    </StyledBurgerMenuOverlay>
+  );
+};
+
+export default BurgerMenu;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,36 +1,87 @@
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
-// import { Link } from 'react-router-dom';
+
+import useResize from '../../hooks/useResize';
+import breakpoints, { media } from '../../styles/breakpoints';
 
 import HrSpaceLogo from '../icons/HrSpaceLogo';
+import BurgerMenuIcon from '../icons/BurgerMenuIcon';
 import HeaderNavigation from '../headerNavigation/HeaderNavigation';
 import HeaderUserInfo from '../headerUserInfo/HeaderUserInfo';
+import BurgerMenu from '../burgerMenu/BurgerMenu';
 
 const StyledHeader = styled.header`
   width: 100%;
   box-sizing: border-box;
   height: 74px;
-  padding: 0 96px;
-  margin: 0;
+  padding: 0 68px;
+  margin: 0 ;
   display: flex;
   justify-content: space-between;
   align-items: center;
   background-color: rgb(0, 0, 0);
+
+  ${media.lg`
+    padding: 0 86px;
+  `}
+
+  ${media.xl`
+    padding: 0 96px;
+  `}
 `;
 
 const StyledLogoWrapper = styled.div`
-  margin-right: calc(224px - 138px);
+  margin: 0;
+
+  ${media.lg`
+    margin-right: calc(224px - 138px);
+  `}
 `;
 
-const Header = () => (
-  <StyledHeader>
-    {/* <Link to="/"> */}
-    <StyledLogoWrapper>
-      <HrSpaceLogo width={138} height={46} />
-    </StyledLogoWrapper>
-    {/* </Link> */}
-    <HeaderNavigation />
-    <HeaderUserInfo />
-  </StyledHeader>
-);
+const Header = () => {
+  const [isBurgerMenuOpen, setIsBurgerMenuOpen] = useState(false);
+  const width = useResize();
+  console.log(width);
+
+  // NOTE: ОБРАБОТЧИКИ КНОПОК ОТКРЫТИЯ И ЗАКРЫТИЯ БУРГЕР-МЕНЮ
+  // NOTE: Обработчик открытия бургер-меню
+  const handleBurgerMenuClick = () => {
+    setIsBurgerMenuOpen(true);
+  };
+
+  // NOTE: Обработчик закрытия бургер-меню
+  const closeBurgerMenu = () => {
+    setIsBurgerMenuOpen(false);
+  };
+
+  useEffect(() => {
+    if (width < parseInt(breakpoints.md, 10)) return;
+    closeBurgerMenu();
+  });
+
+  if (width > parseInt(breakpoints.md, 10)) {
+    return (
+      <StyledHeader>
+        <StyledLogoWrapper>
+          <HrSpaceLogo width={138} height={46} />
+        </StyledLogoWrapper>
+        <HeaderNavigation />
+        <HeaderUserInfo isBurgerMenu={false} />
+      </StyledHeader>
+    );
+  }
+  return (
+    <StyledHeader>
+      <StyledLogoWrapper>
+        <HrSpaceLogo width={138} height={46} />
+      </StyledLogoWrapper>
+      <BurgerMenuIcon onClick={handleBurgerMenuClick} />
+      <BurgerMenu
+        isOpen={isBurgerMenuOpen}
+        onClose={closeBurgerMenu}
+      />
+    </StyledHeader>
+  );
+};
 
 export default Header;
