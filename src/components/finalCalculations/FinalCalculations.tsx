3fc22fdf1c7c3991db137f56864fc710f1ef8228
MaximNabiulin <nabiulin.m.kh@gmail.com>
2024-03-26 00:50:28 +0500

    linter: fix some linter

--- src/components/finalCalculations/FinalCalculations.tsx
+++ src/components/finalCalculations/FinalCalculations.tsx
@@ -35,7 +35,7 @@ const StyledH3 = styled.h3`
   font-size: 14px;
   font-style: normal;
   font-weight: 400;
-  line-height: 140%; 
+  line-height: 140%;
   color: rgba(73, 75, 77, 1);
   margin: 0;
   padding: 0;
@@ -52,7 +52,7 @@ const FinalCalculations: FC<FinalCalculationsProps> = ({
   const formattedFinalAmount = finalAmount?.toLocaleString("ru-RU");
   const awardPerEmployee = () => {
     if (!finalAmount) {
-      return;
+      return undefined;
     }
 
     const formattedAmount = (finalAmount * 0.8).toLocaleString("ru-RU");
@@ -60,7 +60,7 @@ const FinalCalculations: FC<FinalCalculationsProps> = ({
   };
   const serviceFee = () => {
     if (!finalAmount) {
-      return;
+      return undefined;
     }
     const formattedAmount = (finalAmount * 0.2).toLocaleString("ru-RU");
     return formattedAmount;
